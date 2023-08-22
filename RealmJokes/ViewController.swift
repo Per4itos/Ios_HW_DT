ff13ea1a899403f3709ae4acaac2d1414ecbff56
Per4itos <101799073+Per4itos@users.noreply.github.com>
2023-08-22 10:35:23 +0300

    first commit

---
+++ RealmJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmJokes
+//
+//  Created by Адхам Тангиров on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
